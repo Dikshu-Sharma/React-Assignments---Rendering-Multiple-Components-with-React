43520b35c07a5483aa8f75b1108b340391e2362a
silenceofranger <38451055+silenceofranger@users.noreply.github.com>
2020-11-07 01:34:34 +0530

    fds

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,13 @@
-import React, {Component, useState} from "react";
-import '../styles/App.css';
+import React, { Component, useState } from "react";
+import "../styles/App.css";
 
 const App = () => {
   return (
-    <></>
-  )
-}
-
+    <>
+      <span data-ns-test="project-name">CHESS GAME</span>
+      <span data-ns-test="project-description">CHESS GAMBLING WEBSITE</span>
+    </>
+  );
+};
 
 export default App;
